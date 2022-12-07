f17530c8589557a88e4e8ac682bcb1241b3a7e90
dangddoong <dangddoong@gmail.com>
2022-12-07 19:45:00 +0900

    Setting: 프로젝트 생성 및 프론트엔드 초기 세팅

---
+++ src/main/java/com/sparta/spartapost/SpartapostApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartapost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartapostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartapostApplication.class, args);
+	}
+
+}
