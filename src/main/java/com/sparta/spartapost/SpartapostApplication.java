2a80cb17635d263726b89136b512486debec9930
dangddoong <dangddoong@gmail.com>
2022-12-07 22:20:32 +0900

    Feat: SpartapostApplication.java / @EnableJpaAuditing 추가

--- src/main/java/com/sparta/spartapost/SpartapostApplication.java
+++ src/main/java/com/sparta/spartapost/SpartapostApplication.java
@@ -2,8 +2,10 @@ package com.sparta.spartapost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpartapostApplication {
 
 	public static void main(String[] args) {
