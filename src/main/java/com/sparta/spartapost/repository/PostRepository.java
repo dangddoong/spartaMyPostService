966ec21386cade98cee94e3193e4cf0bf364f0f8
dangddoong <dangddoong@gmail.com>
2022-12-07 22:16:25 +0900

    Feat: PostRepository.java / extends JpaRepository<Post, Long>

--- src/main/java/com/sparta/spartapost/repository/PostRepository.java
+++ src/main/java/com/sparta/spartapost/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package com.sparta.spartapost.repository;
 
-public interface PostRepository {
+import com.sparta.spartapost.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
