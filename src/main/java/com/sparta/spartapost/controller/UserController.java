329bac0de5b00ede73ef4993e65a89a3223db4d9
dangddoong <dangddoong@gmail.com>
2022-12-15 20:24:03 +0900

    !HOTFIX: 쵸비상

--- src/main/java/com/sparta/spartapost/controller/UserController.java
+++ src/main/java/com/sparta/spartapost/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
 // @RestController는 @Controller에 @ResponseBody가 추가된 것. 주용도는 Json형태로 객체 데이터를 반환하는 것.
 @RestController
 @RequiredArgsConstructor
@@ -27,7 +29,7 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String signup(@RequestBody SignupRequestDto signupRequestDto) {
+    public String signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         userService.signup(signupRequestDto);
         // return으로 성공했다는 메세지, 상태코드를 반환해야함.
         return "회원가입 성공(리턴값 바꿔줘야합니당)";
--- src/main/java/com/sparta/spartapost/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/spartapost/dto/SignupRequestDto.java
@@ -3,14 +3,17 @@ package com.sparta.spartapost.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 //@Setter  클론 코딩 상에서는 붙어있긴 하지만, 가급적 안쓰려고 노력해봅시다.
 public class SignupRequestDto {
-    private  String username;
-    private  String password;
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "[a-z0-9]",message = "최소 4자 이상, 10자 이하이며 알파벳 소문자와 숫자만 가능합니다")
+    private String username;
+    @Pattern(regexp ="[a-zA-Z0-9]", message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자와 숫자만 가능합니다")
+    @Size(min = 8, max = 15)
+    private String password;
 
-//    public SignupRequestDto(String username, String password) {
-//        this.username = username;
-//        this.password = password;
-//    }
 }
