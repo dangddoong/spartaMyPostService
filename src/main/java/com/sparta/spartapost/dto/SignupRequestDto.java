40326edd0362ac4bdd13c9e5f43b73c53a59f6b9
dangddoong <dangddoong@gmail.com>
2022-12-15 20:49:10 +0900

    Feat: 회원가입시 username, password에 정규식 검증 추가(바로 직전 커밋과 함께 보세요)

--- src/main/java/com/sparta/spartapost/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/spartapost/dto/SignupRequestDto.java
@@ -9,11 +9,11 @@ import javax.validation.constraints.Size;
 @Getter
 //@Setter  클론 코딩 상에서는 붙어있긴 하지만, 가급적 안쓰려고 노력해봅시다.
 public class SignupRequestDto {
-    @Size(min = 4, max = 10)
-    @Pattern(regexp = "[a-z0-9]",message = "최소 4자 이상, 10자 이하이며 알파벳 소문자와 숫자만 가능합니다")
+    @Size(min = 4, max = 10,message = "최소 4자 이상, 10자 이하이며 알파벳 소문자와 숫자만 가능합니다")
+    @Pattern(regexp = "^[a-z0-9]*$",message = "최소 4자 이상, 10자 이하이며 알파벳 소문자와 숫자만 가능합니다")
     private String username;
-    @Pattern(regexp ="[a-zA-Z0-9]", message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자와 숫자만 가능합니다")
-    @Size(min = 8, max = 15)
+    @Pattern(regexp ="^[a-zA-Z0-9]*$", message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자와 숫자만 가능합니다")
+    @Size(min = 8, max = 15,message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자와 숫자만 가능합니다")
     private String password;
 
 }
